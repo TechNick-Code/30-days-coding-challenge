299973448322309b70e3cbba963f17f89716c22b
johnson Nicholas <nicholasjohnson.njt@gmail.com>
2025-11-20 23:45:19 +0100

    working

--- note-app/function.js
+++ note-app/function.js
@@ -1,6 +1,13 @@
+const addNoteButton = document.getElementById('addNoteBtn');
 const cancelNoteButton = document.getElementById('cancelNoteButton');
 const modalOverlay = document.getElementById('modalOverlay');
 
+addNoteButton.addEventListener('click', () => {
+    modalOverlay.style.visibility = 'visible';
+    modalOverlay.style.transition = 'ease-in 33.3s';
+});
+
 cancelNoteButton.addEventListener('click', () => {
-    modalOverlay.style.display = 'none';
+    modalOverlay.style.visibility = 'hidden';
+    modalOverlay.style.transition = 'ease-out 0.3s';
 });
